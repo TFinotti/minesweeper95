6aa90a616ec9b8cdf0bd292b56c8b876d5cac895
Tiago Franco Finotti <52571510+TFinotti@users.noreply.github.com>
2021-02-11 17:43:51 -0500

    Added MinesweeperActions file

--- src/components/Helpers.js
+++ src/components/Helpers.js
@@ -1,109 +1,111 @@
-import _ from "lodash";
+import { flatten } from "lodash";
 
-const { flatten } = _; // lodash
+const helpers = {
+  // assigns props to the cell with id
+  assignCell(state, cellId, props) {
+    const { grid } = state;
+    const cell = this.getCell(state, cellId);
+    const { x, y } = cell;
+    const row = grid[cell.y];
+    return {
+      ...state,
+      grid: [
+        ...grid.slice(0, y),
+        [...row.slice(0, x), { ...cell, ...props }, ...row.slice(x + 1)],
+        ...grid.slice(y + 1),
+      ],
+    };
+  },
 
-// assigns props to the cell with id
-export const assignCell = (state, cellId, props) => {
-  const { grid } = state;
-  const cell = this.getCell(state, cellId);
-  const { x, y } = cell;
-  const row = grid[cell.y];
-  return {
-    // immutable state
-    ...state,
-    grid: [
-      ...grid.slice(0, y),
-      [...row.slice(0, x), { ...cell, ...props }, ...row.slice(x + 1)],
-      ...grid.slice(y + 1),
-    ],
-  };
+  // assign props to all cells in cellIds
+  assignCells(state, cellIds, props) {
+    return {
+      ...state,
+      grid: state.grid.map((row) =>
+        row.map((cell) =>
+          cellIds.includes(cell.id) ? { ...cell, ...props } : cell
+        )
+      ),
+    };
+  },
+
+  // get cell by id
+  getCell(state, cellId) {
+    return flatten(state.grid).find((cell) => cell.id === cellId);
+  },
+
+  // get cell at coordinate x, y
+  getCellAt(state, { x, y }) {
+    const { grid, width, height } = state;
+    if (x >= 0 && y >= 0 && x < width && y < height) {
+      return grid[y][x];
+    }
+    return null;
+  },
+
+  // get cells around cell (where cell.id === cellId)
+  getNeighbors(state, cellId) {
+    const { grid } = state;
+    const { x, y } = this.getCell(state, cellId);
+    return [
+      this.getCellAt(grid, { x: x - 1, y: y - 1 }), // top left
+      this.getCellAt(grid, { x: x, y: y - 1 }), // top
+      this.getCellAt(grid, { x: x + 1, y: y - 1 }), // top right
+
+      this.getCellAt(grid, { x: x - 1, y: y }), // left
+      this.getCellAt(grid, { x: x + 1, y: y }), // right
+
+      this.getCellAt(grid, { x: x - 1, y: y + 1 }), // bottom left
+      this.getCellAt(grid, { x: x, y: y + 1 }), // bottom
+      this.getCellAt(grid, { x: x + 1, y: y + 1 }), // bottom right
+    ].filter((cell) => !!cell);
+  },
+
+  // starting at startCell continuing through its neighbors, finds all cells that touch no mines (returns an array of ids)
+  getRevealCells(state, startCellId, visited = []) {
+    const cell = this.getCell(state, startCellId);
+    if (cell.mine) {
+      return [startCellId];
+    }
+    const neighbors = this.getNeighbors(state, startCellId);
+    const neighborMines = neighbors.filter((cell) => cell.mine);
+    visited.push(startCellId);
+
+    if (neighborMines.length) {
+      return [startCellId];
+    } else {
+      const toReveal = neighbors
+        .filter(
+          (neighbor) => !neighbor.flagged && !visited.includes(neighbor.id)
+        )
+        .map((neighbor) => neighbor.id);
+
+      return flatten([
+        startCellId,
+        ...toReveal.map((cellId) =>
+          this.getRevealCells(state, cellId, visited)
+        ),
+      ]);
+    }
+  },
+
+  // returns the number of mines bordering a cell
+  countMinesAround(state, cellId) {
+    return this.getNeighbors(state, cellId).filter((c) => c.mine).length;
+  },
+
+  // check if you are blown up
+  hasLost(state) {
+    const cells = flatten(state.grid);
+    return cells.some((c) => c.mine && c.revealed);
+  },
+
+  // check if all non-mine cells are revealed
+  hasWon(state) {
+    const cells = flatten(state.grid);
+    const nonMines = cells.filter((c) => !c.mine);
+    return nonMines.every((c) => c.revealed);
+  },
 };
 
-// assign props to all cells in cellIds
-export const assignCells = (state, cellIds, props) => {
-  return {
-    ...state,
-    grid: state.grid.map((row) =>
-      row.map((cell) =>
-        cellIds.includes(cell.id) ? { ...cell, ...props } : cell
-      )
-    ),
-  };
-};
-
-// get cell by id
-export const getCell = (state, cellId) => {
-  return flatten(state.grid).find((cell) => cell.id === cellId);
-};
-
-// get cell at coordinate x, y
-export const getCellAt = (state, { x, y }) => {
-  const { grid, width, height } = state;
-  if (x >= 0 && y >= 0 && x < width && y < height) {
-    return grid[y][x];
-  }
-  return null;
-};
-
-// get cells around cell (where cell.id === cellId)
-export const getNeighbors = (state, cellId) => {
-  const { grid } = state;
-  const { x, y } = this.getCell(state, cellId);
-  return [
-    this.getCellAt(grid, { x: x - 1, y: y - 1 }), // top left
-    this.getCellAt(grid, { x: x, y: y - 1 }), // top
-    this.getCellAt(grid, { x: x + 1, y: y - 1 }), // top right
-
-    this.getCellAt(grid, { x: x - 1, y: y }), // left
-    this.getCellAt(grid, { x: x + 1, y: y }), // right
-
-    this.getCellAt(grid, { x: x - 1, y: y + 1 }), // bottom left
-    this.getCellAt(grid, { x: x, y: y + 1 }), // bottom
-    this.getCellAt(grid, { x: x + 1, y: y + 1 }), // bottom right
-  ].filter((cell) => !!cell); // remove null cells (if target cell is on an edge)
-};
-
-// starting at startCell continuing through its neighbors, finds all cells that touch no mines (returns an array of ids)
-// this is required to open large pockets of empty space when a cell is revealed
-export const getRevealCells = (state, startCellId, visited = []) => {
-  const cell = this.getCell(state, startCellId);
-  if (cell.mine) {
-    // don't reveal empty cells around mines
-    return [startCellId];
-  }
-  const neighbors = this.getNeighbors(state, startCellId);
-  const neighborMines = neighbors.filter((cell) => cell.mine);
-  visited.push(startCellId); // keep track of visited mines (passed to recursive calls)
-
-  if (neighborMines.length) {
-    // stop if the cell borders a mine (but still reveal it)
-    return [startCellId];
-  } else {
-    const toReveal = neighbors
-      .filter((neighbor) => !neighbor.flagged && !visited.includes(neighbor.id)) // unvisited/unflagged neighbors only
-      .map((neighbor) => neighbor.id);
-
-    return flatten([
-      startCellId,
-      ...toReveal.map((cellId) => this.getRevealCells(state, cellId, visited)),
-    ]);
-  }
-};
-
-// returns the number of mines bordering a cell
-export const countMinesAround = (state, cellId) => {
-  return this.getNeighbors(state, cellId).filter((c) => c.mine).length;
-};
-
-// check if you are blown up
-export const hasLost = (state) => {
-  const cells = flatten(state.grid);
-  return cells.some((c) => c.mine && c.revealed);
-};
-
-// check if all non-mine cells are revealed
-export const hasWon = (state) => {
-  const cells = flatten(state.grid);
-  const nonMines = cells.filter((c) => !c.mine);
-  return nonMines.every((c) => c.revealed);
-};
+export default helpers;
---
+++ src/components/MinesweeperActions.js
@@ -0,0 +1,73 @@
+import helpers from "./Helpers";
+import { flatten, sampleSize } from "lodash";
+
+const minesweeperActions = {
+  "@@redux/INIT"(state) {
+    return state;
+  },
+
+  FLAG_CELL(state, { cellId }) {
+    const cell = helpers.getCell(state, cellId);
+    if (cell.revealed) {
+      return state;
+    } else {
+      return helpers.assignCell(state, cellId, {
+        flagged: !cell.flagged,
+      });
+    }
+  },
+
+  // reveal a cell (and all cells around it until a cell containing a mine is reached)
+  REVEAL_CELL(state, { cellId }) {
+    if (state.won || state.lost || helpers.getCell(state, cellId).flagged) {
+      return state;
+    }
+    if (!state.minesPlaced) {
+      state = this.PLACE_MINES(state, { cellIdToAvoid: cellId });
+    }
+    const toReveal = helpers.getRevealCells(state, cellId);
+    const newState = helpers.assignCells(state, toReveal, {
+      revealed: true,
+      flagged: false,
+    });
+    const hasLost = helpers.hasLost(newState);
+    const hasWon = !hasLost && helpers.hasWon(newState);
+    return {
+      ...newState,
+      won: hasWon,
+      lost: hasLost,
+      endTime: Date.now(),
+    };
+  },
+
+  REVEAL_AROUND_CELL(state, { cellId }) {
+    const cell = helpers.getCell(state, cellId);
+    const neighbors = helpers.getNeighbors(state, cellId);
+    const neighborMineCount = neighbors.filter((n) => n.mine).length;
+    const neighborFlagCount = neighbors.filter((n) => n.flagged).length;
+    if (cell.revealed && neighborMineCount === neighborFlagCount) {
+      const nonFlaggedNeighborIds = neighbors
+        .filter((n) => !n.flagged)
+        .map((n) => n.id);
+      return nonFlaggedNeighborIds.reduce(
+        (state, cellId) => this.REVEAL_CELL(state, { cellId }),
+        state
+      );
+    }
+    return state;
+  },
+
+  PLACE_MINES(state, { cellIdToAvoid }) {
+    const validCells = flatten(state.grid)
+      .filter((c) => c.id !== cellIdToAvoid)
+      .map((c) => c.id);
+    const mines = sampleSize(validCells, state.mineCount);
+    return {
+      ...helpers.assignCells(state, mines, { mine: true }),
+      minesPlaced: true,
+      startTime: Date.now(),
+    };
+  },
+};
+
+export default minesweeperActions;
